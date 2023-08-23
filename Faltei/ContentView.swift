c16e084063ba4ebc5071741bd293058cd65d780b
schueda <a.schuedamenezes@gmail.com>
2023-08-23 08:45:06 -0300

    Initial Commit

---
+++ Faltei/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Faltei
+//
+//  Created by André Schueda on 23/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Faltei/FalteiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FalteiApp.swift
+//  Faltei
+//
+//  Created by André Schueda on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct FalteiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
